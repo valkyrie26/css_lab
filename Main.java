9777e7e0a9375a8686a2ff0fd184464db258cbe5
Pankhuri <76050183+valkyrie26@users.noreply.github.com>
2022-04-01 06:40:30 +0530

    Updating main

--- Main.java
+++ Main.java
@@ -8,32 +8,32 @@ public class Main {
 	//Enter practical number
         Scanner sc = new Scanner (System.in);
 
-        System.out.println("Practical Number: ");
+        System.out.println("Enter number: ");
         int prn = sc.nextInt();
 
         switch (prn) {
             case 1:
-                System.out.println("\nPractical 1: Affine Cipher");
+                System.out.println("\n1: Affine Cipher");
                 Affine.affine_main();
                 break;
             case 2:
-                System.out.println("\nPractical 2: Playfair Cipher");
+                System.out.println("\n2: Playfair Cipher");
                 Playfair.playfair_main();
                 break;
             case 3:
-                System.out.println("\nPractical 3: Polynomial Multiplication");
+                System.out.println("\n3: Polynomial Multiplication");
                 Polynomial.polynomial_main();
                 break;
             case 4:
-                System.out.println("\nPractical 4: Primitive Roots");
+                System.out.println("\n4: Primitive Roots");
                 PrimitiveRoots.primitiveroots_main();
                 break;
             case 5:
-                System.out.println("\nPractical 5: RSA Algorithm");
+                System.out.println("\n5: RSA Algorithm");
                 RSA.rsa_main();
                 break;
             case 7:
-                System.out.println("\nPractical 7: Diffie-Hellman Key Exchange");
+                System.out.println("\n7: Diffie-Hellman Key Exchange");
                 DiffieHellman.diffie_main();
                 break;
             default:
